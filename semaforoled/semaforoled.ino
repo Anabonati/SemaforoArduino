0a2d751ec099c4e6677f6231e1cb4b24b251da20
AnaBonati <124581821+Anabonati@users.noreply.github.com>
2024-02-29 15:24:43 -0300

    Versão 1

---
+++ semaforoled/semaforoled.ino
@@ -0,0 +1,27 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(13, LOW);
+  delay(0);  
+  
+   digitalWrite(12, HIGH);
+  delay(500); 
+  digitalWrite(12, LOW);
+  delay(0);  
+  
+   digitalWrite(11, HIGH);
+  delay(1000); 
+  digitalWrite(11, LOW);
+  delay(0);  
+  
+}
